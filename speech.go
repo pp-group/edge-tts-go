a9a020f482bf253c406472dfb44d832812feb2eb
csh0101 <1945598041@qq.com>
2023-07-17 01:07:12 +0800

    chore: replace os.file with io.ReadWriter

--- speech.go
+++ speech.go
@@ -1,42 +1,20 @@
 package edge_tts_go
 
 import (
-	"crypto/sha256"
-	"encoding/hex"
 	"fmt"
-	"os"
+	"io"
 
 	"github.com/pp-group/edge-tts-go/biz/service/tts/edge"
 )
 
 type Speech struct {
 	*edge.Communicate
-	file     *os.File
-	Folder   string
-	fileName string
-}
-
-func (s *Speech) GetFileName() string {
-	return s.fileName
+	Rw io.ReadWriter
 }
 
 func (s *Speech) GenTTS() error {
-	fileName := s.Folder + "/" + generateHashName(s.Text, s.VoiceLangRegion) + ".mp3"
-	s.fileName = fileName
-	if s.isSpeechExist(fileName) {
-		return nil
-	}
-	err := s.createFile(fileName)
-	if err != nil {
-		return err
-	}
-	defer s.file.Close()
-
-	err = s.gen()
-	if err != nil {
-		return err
-	}
-	return nil
+	// fileName := s.Folder + "/" + generateHashName(s.Text, s.VoiceLangRegion) + ".mp3"
+	return s.gen()
 }
 
 func (s *Speech) gen() error {
@@ -67,38 +45,10 @@ func (s *Speech) gen() error {
 	// write data, sort by index
 	for _, v := range audioData {
 		for _, data := range v {
-			s.file.Write(data)
+			if _, err := s.Rw.Write(data); err != nil {
+				return err
+			}
 		}
 	}
 	return nil
-}
-
-func (s *Speech) isSpeechExist(fileName string) bool {
-	file, err := os.Open(fileName)
-	if os.IsNotExist(err) {
-		return false
-	}
-	file.Close()
-	return true
-}
-
-func (s *Speech) createFile(fileName string) error {
-	// if file exist than return
-	// else create it
-	file, err := os.Open(fileName)
-	if err == nil {
-		return nil
-	} else {
-		file, err = os.Create(fileName)
-		if err != nil {
-			return err
-		}
-	}
-	s.file = file
-	return nil
-}
-
-func generateHashName(name, voice string) string {
-	hash := sha256.Sum256([]byte(name))
-	return fmt.Sprintf("%s_%s", voice, hex.EncodeToString(hash[:]))
 }
--- speech_test.go
+++ speech_test.go
@@ -1,34 +1,52 @@
 package edge_tts_go
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"os"
 	"testing"
 
 	"github.com/pp-group/edge-tts-go/biz/service/tts/edge"
 )
 
 func TestGenTTS(t *testing.T) {
-	fileName, err := genTTS("cpdd,cpdd,cpdd")
+	dir := "x"
+	text := "cpdd,cpdd,cpdd"
+	fileName := generateHashName(text, "zh-CN-XiaoxiaoNeural")
+	f, err := os.OpenFile(fmt.Sprintf("%s/%s.mp3", dir, fileName), os.O_CREATE|os.O_APPEND|os.O_RDWR, os.ModePerm)
 	if err != nil {
+		t.Errorf("open file error %v", err)
+		return
+	}
+	defer f.Close()
+
+	if err := genTTS(text, f); err != nil {
 		t.Errorf("genTTS fail, err: %v", err)
 		return
 	}
 	t.Logf("genTTS success, fileName: %s", fileName)
 }
 
+func generateHashName(name, voice string) string {
+	hash := sha256.Sum256([]byte(name))
+	return fmt.Sprintf("%s_%s", voice, hex.EncodeToString(hash[:]))
+}
+
 // genTTS template use github.com/pp-group/edge-tts-go
-func genTTS(text string) (string, error) {
+func genTTS(text string, rw io.ReadWriter) error {
 	c, err := edge.NewCommunicate(text)
 	if err != nil {
-		return "", err
+		return err
 	}
-
 	speech := &Speech{
 		Communicate: c,
-		Folder:      "templateaudio",
+		Rw:          rw,
 	}
 	err = speech.GenTTS()
 	if err != nil {
-		return "", err
+		return err
 	}
-	return speech.GetFileName(), nil
+	return nil
 }
